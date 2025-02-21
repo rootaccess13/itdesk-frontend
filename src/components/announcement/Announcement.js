10c3eed1fefe7743ef879259f2d5d4f0b169f748
rootaccess13 <43539755+rootaccess13@users.noreply.github.com>
2025-02-21 18:52:58 +0800

    Update Announcement.js

--- src/components/announcement/Announcement.js
+++ src/components/announcement/Announcement.js
@@ -103,7 +103,7 @@ const Announcement = () => {
   };
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex min-h-screen bg-gray-50">
       <SidebarComponent />
       <div className="flex-1 p-6">
         <div className="max-w-7xl mx-auto">
